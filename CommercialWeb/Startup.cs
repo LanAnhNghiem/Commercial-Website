d6919db84ff3f0a029ceff3ee2b76af2cf6f6e79
LanAnhNghiem <lananh.nghiem0502@gmail.com>
2017-11-24 14:20:04 +0700

    Database and Layout

---
+++ CommercialWeb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(CommercialWeb.Startup))]
+namespace CommercialWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            //ConfigureAuth(app);
+        }
+    }
+}
