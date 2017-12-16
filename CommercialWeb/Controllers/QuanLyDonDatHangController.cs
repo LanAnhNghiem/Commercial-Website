ee2de8afe153b480c9fc7f8f1de0a8da64c602bd
Thanh_Chuong <nguyenvietthanhchuong@gmail.com>
2017-12-16 08:34:05 +0700

    Add Quản lý đơn hàng Controller và View quản lý đơn hàng

---
+++ CommercialWeb/Controllers/QuanLyDonDatHangController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using CommercialWeb.Models;
+
+namespace CommercialWeb.Controllers
+{
+    public class QuanLyDonDatHangController : Controller
+    {
+        QuanLyBanHangEntities db = new QuanLyBanHangEntities();
+        // GET: QuanLyDonDatHang
+        public ActionResult QuanLyDonHang()
+        {
+            return View(db.DonHangs.Where(n=>n.MaTinhTrang == 1));
+        }
+    }
+}
