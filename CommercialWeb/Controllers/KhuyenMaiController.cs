d7c93885c9e39c3de5b74510e7eded7489482527
Thanh_Chuong <nguyenvietthanhchuong@gmail.com>
2017-12-24 19:25:45 +0700

    Hoàn thành tạm thời chức năng khuyến mãi

--- CommercialWeb/Controllers/KhuyenMaiController.cs
+++ CommercialWeb/Controllers/KhuyenMaiController.cs
@@ -36,15 +36,48 @@ namespace CommercialWeb.Controllers
         public ActionResult ChinhSuaKhuyenMai(int? MaKhuyenMai)
         {
             KhuyenMai km = db.KhuyenMais.SingleOrDefault(p => p.MaKhuyenMai == MaKhuyenMai);
+            ViewBag.lstSanPham = db.SanPhams.Where(n => n.MaKhuyenMai == MaKhuyenMai);
+            ViewBag.lstSanPhamBT = db.SanPhams.Where(n => n.MaKhuyenMai == 1); 
             return View(km);
         }
 
         [HttpPost]
-        public ActionResult ChinhSuaKhuyenMai(KhuyenMai khuyenmai)
-        { 
-            return View();
+        public ActionResult ChinhSuaKhuyenMai(KhuyenMai km, IEnumerable<SanPham> lstSPKM)
+        {
+            KhuyenMai kmUpdate = db.KhuyenMais.SingleOrDefault(n => n.MaKhuyenMai == km.MaKhuyenMai);
+            kmUpdate.TenKhuyenMai = km.TenKhuyenMai;
+            kmUpdate.MoTa = km.MoTa;
+            db.SaveChanges();
+
+            //Huy khuyen mai tat ca san pham dang khuyen mai tuong ung
+            IEnumerable<SanPham> lstSPdangKM = db.SanPhams.Where(n => n.MaKhuyenMai == km.MaKhuyenMai);
+            foreach(var sp in lstSPdangKM)
+            {
+                sp.MaKhuyenMai = 1;
+                sp.GiaBan = sp.DonGia;
+            }
+            db.SaveChanges();
+            //Cập nhật lại danh sách sản phẩm khuyến mãi mới
+            SanPham spUpdate;
+            if(lstSPKM != null)
+            {
+                foreach (var sp in lstSPKM)
+                {
+                    spUpdate = db.SanPhams.SingleOrDefault(n => n.MaSP == sp.MaSP);
+                    spUpdate.MaKhuyenMai = km.MaKhuyenMai;
+                    spUpdate.GiaBan = spUpdate.DonGia * (1 - (decimal)km.MoTa / 100);
+                }
+            }
+
+            db.SaveChanges();
+
+            ViewBag.lstSanPham = db.SanPhams.Where(n => n.MaKhuyenMai == km.MaKhuyenMai);
+            ViewBag.lstSanPhamBT = db.SanPhams.Where(n => n.MaKhuyenMai == 1);
+            ViewBag.ThongBao = "Thành công !";
+            return View(kmUpdate);
         }
 
+        
         public ActionResult HuyKhuyenMai(int? MaKhuyenMai)
         {
             KhuyenMai kmupdate = db.KhuyenMais.SingleOrDefault(p => p.MaKhuyenMai == MaKhuyenMai);
@@ -52,6 +85,7 @@ namespace CommercialWeb.Controllers
             foreach (var sp in lstSP)
             {
                 sp.MaKhuyenMai = 1;
+                sp.GiaBan = sp.DonGia;
             }
             kmupdate.DaHuy = true;
             db.SaveChanges();
@@ -62,6 +96,7 @@ namespace CommercialWeb.Controllers
         {
             SanPham spUpdate = db.SanPhams.SingleOrDefault(p => p.MaSP == MaSP);
             spUpdate.MaKhuyenMai = 1;
+            spUpdate.GiaBan = spUpdate.DonGia;
             db.SaveChanges();
             return RedirectToAction("CacSanPhamKhuyenMai");
         }
@@ -77,6 +112,7 @@ namespace CommercialWeb.Controllers
         public ActionResult ThemMoiKhuyenMai()
         {
             ViewBag.lstSanPham = db.SanPhams.Where(n=>n.MaKhuyenMai == 1);
+            db.SaveChanges();
             return View();
         }
 
@@ -84,6 +120,18 @@ namespace CommercialWeb.Controllers
         public ActionResult ThemMoiKhuyenMai(KhuyenMai km, IEnumerable<SanPham> lstSP)
         {
             ViewBag.lstSanPham = db.SanPhams.Where(n => n.MaKhuyenMai == 1);
+            //Cap nhat gia va ma khuyen mai cho tat ca sp dc ap dung khuyen mai
+            SanPham spUpdate;
+            foreach(var sp in lstSP)
+            {
+                spUpdate = db.SanPhams.SingleOrDefault(n => n.MaSP == sp.MaSP);
+                spUpdate.MaKhuyenMai = km.MaKhuyenMai;
+                spUpdate.GiaBan = sp.DonGia * (1 - (decimal)km.MoTa/100);
+            }
+            km.DaHuy = false;
+            db.KhuyenMais.Add(km);
+            db.SaveChanges();
+            ViewBag.ThongBao = "Thành công !";
             return View();
         }
     }
