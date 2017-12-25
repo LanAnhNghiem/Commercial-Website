4e1d0b0bb6ccc345cb5b6315d82f94cafab0a92b
Thanh_Chuong <nguyenvietthanhchuong@gmail.com>
2017-12-25 16:55:23 +0700

    Thêm View Đăng nhập

---
+++ CommercialWeb/Controllers/DangNhapController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CommercialWeb.Controllers
+{
+    public class DangNhapController : Controller
+    {
+        // GET: DangNhap
+        public ActionResult DangNhap()
+        {
+            return View();
+        }
+    }
+}
