2577f48072a84dcb07756ab86ad3e5cf2cac6f14
Thanh_Chuong <nguyenvietthanhchuong@gmail.com>
2017-12-26 13:26:32 +0700

    Hoàn thành cơ bản Đăng nhập

--- CommercialWeb/Controllers/DangNhapController.cs
+++ CommercialWeb/Controllers/DangNhapController.cs
@@ -3,22 +3,46 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
-
+using CommercialWeb.Models;
 namespace CommercialWeb.Controllers
 {
     public class DangNhapController : Controller
     {
+        QuanLyBanHangEntities db = new QuanLyBanHangEntities();
         // GET: DangNhap
+        /// <summary>
+        /// Chức năng đăng nhập
+        /// Creator:Chương
+        /// </summary>
+        /// <returns>Trang đăng nhập</returns>
         [HttpGet]
         public ActionResult DangNhap()
         {
             return View();
         }
 
+        /// <summary>
+        /// Chức năng đăng nhập
+        /// Creator: Chương
+        /// </summary>
+        /// <param name="email">Nhận vào email</param>
+        /// <param name="password">Nhận vào password</param>
+        /// <returns>Điều hướng tới trang đơn hàng chưa giao</returns>
         [HttpPost]
-        public ActionResult DangNhap(int i)
+        public ActionResult DangNhap(string email, string password)
         {
+            ThanhVien tv = db.ThanhViens.SingleOrDefault(n => n.Email == email && n.MatKhau == password);
+            if (true)
+            {
+                Session["TaiKhoan"] = tv;
+            }
             return RedirectToAction("ChuaGiao", "QuanLyDonHang");
+        }
+        
+        public ActionResult DangXuat()
+        {
+            Session["TaiKhoan"] = null;
+            return View("DangNhap");
         }
     }
 }
